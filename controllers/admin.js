e1d0ff96f12c5030f97b86289514a9d6aa1e7382
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-06-22 21:53:45 +0700

    Delete data from db

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProduct = (req, res, next) => {
   })
     .then((result) => {
       console.log(result);
+      res.redirect("/admin/products");
     })
     .catch((err) => {
       console.log(err);
@@ -93,6 +94,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log("Product Deleted");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
