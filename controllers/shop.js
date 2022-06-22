348adba60287e25612f8ce109df69638e287a066
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-06-22 10:49:52 +0700

    Fetching data from db

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,21 +2,26 @@ const Cart = require("../models/Cart");
 const Product = require("../models/Product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
-    res.render("shop/product-list", {
-      prods: rows,
-      pageTitle: "Products",
-      path: "/products",
+  Product.findAll()
+    .then((products) => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 exports.getDetailProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
@@ -25,10 +30,10 @@ exports.getDetailProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
       });
